0a7b643618a51ead91466d54481572ccd6d59268
BrenoJLeal <140741784+BrenoJLeal@users.noreply.github.com>
2024-04-22 22:43:48 -0300

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,30 @@
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
+
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', () => {
+        menuShow(); 
+    });
+});
+
+function carousel() {
+    var counter = 1;
+    setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 3){
+            counter = 1;
+        }
+    }, 4000);
+}
+
+carousel();
