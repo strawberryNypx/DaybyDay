56802551daedd9689aa25ab80ad8bfcbfa66887a
Clara Ferraz <claraferraz@proton.me>
2024-04-29 20:54:32 -0300

    Feat: sobre e dropdown menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,30 +1,43 @@
-
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+  }
 }
 
-document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('click', () => {
-        menuShow(); 
-    });
+document.querySelectorAll(".nav-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    menuShow();
+  });
 });
 
 function carousel() {
-    var counter = 1;
-    setInterval(function(){
-        document.getElementById('radio' + counter).checked = true;
-        counter++;
-        if(counter > 3){
-            counter = 1;
-        }
-    }, 4000);
+  var counter = 1;
+  setInterval(function () {
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if (counter > 3) {
+      counter = 1;
+    }
+  }, 7000);
 }
 
 carousel();
+
+//dropdonw do Sobre no menu
+function menuDropdown() {
+  const dropContent = document.querySelector(".drop-content");
+  if (dropContent.classList.contains("open")) {
+    dropContent.classList.remove("open");
+    document.querySelector(".drop-icon").src = "assets/img/down-arrow.png";
+  } else {
+    dropContent.classList.add("open");
+    document.querySelector(".drop-icon").src = "assets/img/up-arrow.png";
+  }
+}
+
+//dropdown das perguntas
